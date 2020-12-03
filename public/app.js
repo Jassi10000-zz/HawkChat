2ff6e899a722dd8715068077383970a0ed09a866
JASKEERAT SINGH <jaskeerat10000@gmail.com>
2020-12-03 22:47:55 +0530

    first commit

---
+++ public/app.js
@@ -0,0 +1,90 @@
+const socket = io();
+
+const msgText = document.querySelector('#msg')
+const btnSend = document.querySelector('#btn-send')
+const chatBox = document.querySelector('.chat-content')
+const displayMsg = document.querySelector('.message')
+
+let name;
+// do{
+//     name = prompt("What's ur name ? ")
+// }
+// while(!name)
+// document.querySelector('#name').textContent = name;
+// msgText.focus();
+
+var userTwoName;
+
+function getName(){
+    name = prompt("What's ur name ? ")
+    document.querySelector('#name').textContent = name;
+    msgText.focus();
+
+
+    socket.emit("sending name" , name || "user")  
+}
+
+btnSend.addEventListener('click' , (e) => {
+    e.preventDefault()
+    sendMsg(msgText.value);
+    msgText.value = '';
+    msgText.focus()
+    chatBox.scrollTop = chatBox.scrollHeight;
+}) 
+
+
+const sendMsg = message => {
+    let msg = {
+        name: name,
+        message: message.trim()
+    }
+
+    display(msg, 'my-message', name)
+
+    socket.emit('sendMessage' , msg)
+
+}
+
+socket.on('sendToAll' , msg => { 
+    display(msg , 'other-message', userTwoName)
+})
+socket.on('sendNameToAll' , obj => { 
+    console.log(obj);
+    // display(name , 'other-message')
+    userTwoName = obj.name;
+    let tempIndex = obj.users.findIndex(i=>i==name)
+    console.log(tempIndex)
+
+    if(tempIndex == 0){
+        userTwoName = obj.users[1];
+    }
+    else{
+        userTwoName = obj.users[0];
+    }
+})
+
+getName();
+
+
+const display = (msg, type, displayName) =>{
+    const msgDiv = document.createElement('div')
+    let className = type
+    msgDiv.classList.add(className, 'message-row')
+    let times = new Date().toLocaleTimeString()
+
+    let innerText = `
+    <div class="message-title">
+    👲<span>${displayName}</span>
+    </div>
+    <div class="message-text">
+        ${msg.message}
+    </div>
+    <div class="message-time">
+        ${times}
+    </div>
+    `;
+
+    msgDiv.innerHTML = innerText;
+    displayMsg.appendChild(msgDiv)
+    chatBox.scrollTop = chatBox.scrollHeight;
+}
---
+++ server.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+__dirname__= fs.realpathSync('.');
+
+const express = require('express');
+
+const path = require('path');
+
+
+const app = express();
+
+const http = require('http').createServer(app);
+
+
+
+app.use(express.static(path.join(__dirname__, 'public')))
+
+let users = [];
+
+const io = require('socket.io')(http)
+io.on('connection' , socket=>{
+   console.log("Connection ready");
+
+   socket.on('sending name' , name=>{
+
+      if(users.length >= 2){
+         console.log("Not allowed")
+         return;
+      }
+      
+      console.log(name)
+      users.push(name);
+      io.emit('sendNameToAll' , {name,users})
+      
+      console.log(users)
+   }) 
+
+   socket.on('sendMessage' , msg=>{
+      socket.broadcast.emit('sendToAll' , msg)
+   }) 
+})
+
+
+
+const PORT = 3000
+
+// process.env is a J.S object
+
+http.listen(PORT , () => {
+   console.log("Server listening at port" + PORT); 
+})
